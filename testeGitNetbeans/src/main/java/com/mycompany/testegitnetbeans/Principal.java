99ff7e4cabbcfe16f26f65224fc8cff5eff4abf8
Molico Junior <molico@yahoo.com>
2025-06-14 15:36:29 -0300

    Acrescimo das variaveis "nome" e "idade"

--- testeGitNetbeans/src/main/java/com/mycompany/testegitnetbeans/Principal.java
+++ testeGitNetbeans/src/main/java/com/mycompany/testegitnetbeans/Principal.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.testegitnetbeans;
 
+import java.util.Scanner;
+
 /**
  *
  * @author molico
@@ -12,7 +14,12 @@ public class Principal {
 
     public static void main(String[] args) {
         
-        Pessoa pessoa01 = new Pessoa("Darth Vader", 30);
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o nome da pessoa: ");
+        String nome = teclado.next();
+        System.out.println("Digite a idade da pessoa: ");
+        int idade = teclado.nextInt();
+        Pessoa pessoa01 = new Pessoa(nome,idade);
         System.out.println(pessoa01.getNome() + " tem " + pessoa01.getIdade() + " anos.");
         System.out.println("Essa pessoa fez aniversário");
         pessoa01.fazerAniversario();
